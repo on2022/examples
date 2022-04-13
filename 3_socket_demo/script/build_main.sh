12ea4aa40f964adde6f59501afc499c54ba1ff76
huangdizhen <18826075849@163.com>
2022-04-13 23:34:33 +0800

    [socket] add socket demo.

---
+++ 3_socket_demo/script/build_main.sh
@@ -0,0 +1,10 @@
+root=`cd $(dirname $0);pwd`/..
+echo $root
+
+cd $root
+rm -rf build
+mkdir build && cd build && cmake ..  \
+&&  make -j$(nproc)
+
+
+
---
+++ 3_socket_demo/src/config.h
@@ -0,0 +1,20 @@
+/*
+ * config.h 包含该tcp/ip套接字编程所需要的基本头文件，与server.c client.c位于同一目录下
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <errno.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+
+const int MAX_LINE = 2048;  
+const int PORT = 6000;      // 服务器sokcet 端口号
+const int LISTENQ = 6666;   // 服务器监听请求次数
+
+
